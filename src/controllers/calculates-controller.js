3db3e98b8031d075baf39670bd4b7251fda05b3b
jvivero <jesus.vivero84@gmail.com>
2022-05-30 06:24:11 -0500

    Optimizacion de codigo en controlador de calculos

--- src/controllers/calculates-controller.js
+++ src/controllers/calculates-controller.js
@@ -5,14 +5,15 @@ const { getCurrentTeamLevel } = require('../data/teams-levels');
 const controller = {};
 
 
-// controlador para calcular el salario de los jugadores
-controller.calculatePlayerSalary = (req, res, next) => {
 
-  // Debido a que el codigo fuente esta en ingles y el JSON está en espanol, se extraen los valores en variables para no mesclar el ingles con el espanol
-  const players = req.body.jugadores; // Se obtiene el objeto con la información del JSON de datos de entrada
+
+
+// Proceso para calcular los sueldos de la lista de jugadores
+const calculateSalaries = (players, teamName) => {
+
   //
-  const teams = new Object(); // Objeto de niveles por equipos
-  let playersResult; // Array de jugadores mutable
+  const teamsTotals = new Object(); // Objeto de niveles por equipos
+  let playersResult; // Para el Array de jugadores mutable
 
   // Ciclo para calcular el puntaje global por equipo y el procentaje personal del jugador
   playersResult = players.map((player, index) => {
@@ -20,12 +21,12 @@ controller.calculatePlayerSalary = (req, res, next) => {
     // Debido a que el codigo fuente esta en ingles y el JSON está en espanol, se extraen los valores en variables para no mesclar el ingles con el espanol
     const playerGoals = player.goles;
     const playerLevel = player.nivel;
-    const playerTeam = player.equipo;
+    const playerTeam = player.equipo || teamName; // Si el jugador no tiene el equipo se toma el parametro de entrada
     //
     const currentTeamLevel = getCurrentTeamLevel(playerTeam, playerLevel); // Se obtiene el nivel del jugador dentro de los niveles de su equipo
 
-    if (!teams[playerTeam]) { // Si no esta definido el equipo, se establece el objeto con sus propiedades
-      teams[playerTeam] = {
+    if (!teamsTotals[playerTeam]) { // Si no esta definido el equipo en el objeto de acumulados totales, se establece el objeto con sus propiedades
+      teamsTotals[playerTeam] = {
         totalPlayerGoals: 0,
         totalGoalsLevels: 0,
         get getGoalsPercentage() { // retorna el porcentaje de goles de jugadores del equipo por nivel
@@ -34,9 +35,9 @@ controller.calculatePlayerSalary = (req, res, next) => {
       };
     }
 
-    // Acumular valores
-    teams[playerTeam].totalPlayerGoals = (teams[playerTeam].totalPlayerGoals || 0) + playerGoals; // total goles de los jugadores dentro del equipo
-    teams[playerTeam].totalGoalsLevels = (teams[playerTeam].totalGoalsLevels || 0) + currentTeamLevel.goals; // total de meta de goles de los jugadores segun nivel del equipo
+    // Acumular valores totales por equipo
+    teamsTotals[playerTeam].totalPlayerGoals = (teamsTotals[playerTeam].totalPlayerGoals || 0) + playerGoals; // total goles de los jugadores dentro del equipo
+    teamsTotals[playerTeam].totalGoalsLevels = (teamsTotals[playerTeam].totalGoalsLevels || 0) + currentTeamLevel.goals; // total de meta de goles de los jugadores segun nivel del equipo
 
     const goalsPercentage = playerGoals / currentTeamLevel.goals; // Porcentaje de goles del jugador segun nivel del equipo
 
@@ -47,8 +48,8 @@ controller.calculatePlayerSalary = (req, res, next) => {
 
   });
 
-  //console.log(teams);
-  /*Object.defineProperties(teams[], {
+  //console.log(teamsTotals);
+  /*Object.defineProperties(teamsTotals[], {
     'getPercentage': {
       get: function () {
         return this.totalPlayerGoals / this.totalGoalsLevels
@@ -60,12 +61,12 @@ controller.calculatePlayerSalary = (req, res, next) => {
   playersResult = playersResult.map((player, index) => {
 
     // Debido a que el codigo fuente esta en ingles y el JSON está en espanol, se extraen los valores en variables para no mesclar el ingles con el espanol
-    const currentPlayer = player;
+    const currentPlayer = Object.assign({}, player); // Para copiar el objeto y no la referencia (Solo si el objeto no posee subojetos)
     const playerSalary = currentPlayer.sueldo;
     const playerBonus = currentPlayer.bono;
-    const playerTeam = currentPlayer.equipo;
+    const playerTeam = currentPlayer.equipo || teamName;
     //
-    const bonusPercentage = (teams[playerTeam].getGoalsPercentage + currentPlayer.goalsPercentage) / 2; // Se calcula el procentaje del bono 50% equipo 50% personal
+    const bonusPercentage = (teamsTotals[playerTeam].getGoalsPercentage + currentPlayer.goalsPercentage) / 2; // Se calcula el procentaje del bono 50% equipo 50% personal
 
     const bonus = customRound(playerBonus * bonusPercentage, 2); // Calculo del bono del jugador
 
@@ -78,10 +79,28 @@ controller.calculatePlayerSalary = (req, res, next) => {
 
   });
 
-  res.json(playersResult); // Retorna la respuesta
-  //res.json(teams);
+  return playersResult;
+
+}; // end: const calculateSalary = (players, teamName) => {
+
+
+
+
+
+// controlador para calcular el salario de los jugadores
+controller.calculatePlayerSalary = (req, res, next) => {
+
+  // Debido a que el codigo fuente esta en ingles y el JSON está en espanol, se extraen los valores en variables para no mesclar el ingles con el espanol
+  const players = req.body.jugadores; // Se obtiene el objeto con la información del JSON de datos de entrada
+  //
+  const playersResult = calculateSalaries(players, null); // Calcular los sueldos de la lista de jugadores
+
+  res.json(playersResult); // Retorna la respuesta al frontend
 
 } // end: controller.calculatePlayerSalary = (req, res, next) => {
+
+
+
 
 
 // Controlador para calcular el salario de los jugadores por arreglo de equipos
@@ -89,84 +108,29 @@ controller.calculateTeamsSalary = (req, res, next) => {
 
   // Debido a que el codigo fuente esta en ingles y el JSON está en espanol, se extraen los valores en variables para no mesclar el ingles con el espanol
   const teams = req.body.equipos; // Se obtiene el objeto con la información del JSON de datos de entrada
-  //
-  let teamsResult;
 
-  teamsResult = teams.map((team, index) => {
+  // Recorrer la lista de equipos recibida
+  const teamsResult = teams.map((team, index) => {
 
     // Debido a que el codigo fuente esta en ingles y el JSON está en espanol, se extraen los valores en variables para no mesclar el ingles con el espanol
     const players = team.jugadores;
-    let totalPlayerGoals = 0;
-    let totalGoalsLevels = 0;
+    const teamName = team.nombre;
 
-    const playersResult = players.map((player, index) => {
-      const playerGoals = player.goles;
-      const playerLevel = player.nivel;
-      const playerTeam = team.nombre;
-      //
-      const currentTeamLevel = getCurrentTeamLevel(playerTeam, playerLevel); // Se obtiene el nivel del jugador dentro de los niveles de su equipo
-
-
-      // Acumular valores
-      totalPlayerGoals += playerGoals; // total goles de los jugadores dentro del equipo
-      totalGoalsLevels += currentTeamLevel.goals; // total de meta de goles de los jugadores segun nivel del equipo
-
-      const goalsPercentage = playerGoals / currentTeamLevel.goals; // Porcentaje de goles del jugador segun nivel del equipo
-
-      return { // Retorna el objeto con los datos del jugador
-        ...player,
-        goalsPercentage // Se agrega nueva propiedad al jugador para usar en el calculo del bono posteriormente
-      };
-    });
+    const playersResult = calculateSalaries(players, teamName); // Calcular los sueldos de la lista de jugadores
 
     return {
-      ...team,
-      jugadores: playersResult,
-      totalPlayerGoals,
-      totalGoalsLevels,
-      get getGoalsPercentage() { // retorna el porcentaje de goles de jugadores del equipo por nivel
-        return /*customRound(*/this.totalPlayerGoals / this.totalGoalsLevels/*, 4)*/;
-      }
+      ...team, // Los datos del equipo que no cambiaron
+      jugadores: playersResult // La nueva lista de jugadores con los sueldos calculados
     };
 
   });
 
-  teamsResult = teamsResult.map((team, index) => {
+  res.json(teamsResult); // Retorna la respuesta al frontend
 
-    const currentTeam = team;
-    const players = currentTeam.jugadores;
-    // Ciclo para calcular la bonificación y el sueldo de cada jugador
-    const playersResult = players.map((player, index) => {
-
-      // Debido a que el codigo fuente esta en ingles y el JSON está en espanol, se extraen los valores en variables para no mesclar el ingles con el espanol
-      const currentPlayer = player;
-      const playerSalary = currentPlayer.sueldo;
-      const playerBonus = currentPlayer.bono;
-      //
-      const bonusPercentage = (currentTeam.getGoalsPercentage + currentPlayer.goalsPercentage) / 2; // Se calcula el procentaje del bono 50% equipo 50% personal
-
-      const bonus = customRound(playerBonus * bonusPercentage, 2); // Calculo del bono del jugador
-
-      delete currentPlayer.goalsPercentage; // Se borrar la propiedad despues de ser usada en el calculo y la cual no necesita mostrarse en la salida
-
-      return {
-        ...currentPlayer, // Retorna el objeto con los datos del jugador
-        sueldo_completo: customRound(playerSalary + bonus, 2) // Se calcula el sueldo total del jugador
-      }
-    });
-
-    delete currentTeam.totalPlayerGoals;
-    delete currentTeam.totalGoalsLevels;
-    delete currentTeam.getGoalsPercentage;
-
-    return {
-      ...currentTeam,
-      jugadores: playersResult
-    };
-
-  });
-
-  res.json(teamsResult);
 }; // end: controller.calculateTeamsSalary = (req, res, next) => {
 
-module.exports = controller;
+
+
+
+
+module.exports = controller; // Exportar el controlador
